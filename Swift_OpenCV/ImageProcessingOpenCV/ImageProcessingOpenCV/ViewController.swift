7b735e07e2120bc916cf0ba33423f9c137a3c2fa
Mariia Turchina <mariia.turchina@gmail.com>
2019-10-11 11:29:55 +0200

    added video fetch

--- Swift_OpenCV/ImageProcessingOpenCV/ImageProcessingOpenCV/ViewController.swift
+++ Swift_OpenCV/ImageProcessingOpenCV/ImageProcessingOpenCV/ViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import AVKit
+
 import AVFoundation
 import Foundation
 
-class ViewController: UIViewController, OpenCVCamDelegate {
+class ViewController: UIViewController, OpenCVCamDelegate, AVAssetResourceLoaderDelegate {
     
     let advice_lib : [String : String] = ["0" : "not_detected",
                                           "1" : "detected",
@@ -36,6 +38,12 @@ class ViewController: UIViewController, OpenCVCamDelegate {
     
     var audioPlayer: AVAudioPlayer? = nil
     var currentAudio : String = "none"
+    
+    var player : AVPlayer?
+    var output : AVPlayerItemVideoOutput!
+    
+    @IBOutlet weak var image_h264: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,12 +63,60 @@ class ViewController: UIViewController, OpenCVCamDelegate {
         openCVWrapper.start()
         
         playAdvice()
+        
+        // H264 decoding
+        //let videoURL = Bundle.main.url(forResource: "foxVillage", withExtension: "m3u8")
+        //let url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        //let url =  "https://wolverine.raywenderlich.com/content/ios/tutorials/video_streaming/foxVillage.m3u8"
+        //let url = "https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4"
+        let url = "http://192.168.1.121:8080/video"
+        //let url = "rtsp://192.168.1.121:8080/h264_pcm.sdp"
+        let videoURL = URL(string: url)
+        let asset = AVURLAsset(url: videoURL!, options: nil)
+        let playerItem = AVPlayerItem(asset: asset)
+        player = AVPlayer(playerItem: playerItem)
+            
+             let bufferAttributes: [String: Any] = [
+                       String(kCVPixelBufferPixelFormatTypeKey): Int(kCVPixelFormatType_32BGRA),
+                       String(kCVPixelBufferIOSurfacePropertiesKey): [String: AnyObject](),
+                       String(kCVPixelBufferOpenGLCompatibilityKey): true
+                   ]
+                   
+            output = AVPlayerItemVideoOutput(pixelBufferAttributes: bufferAttributes)
+            output.suppressesPlayerRendering = true
+            
+            player?.currentItem?.add(output)
+            player?.play()
     }
 
+    func resourceLoader(_ resourceLoader: AVAssetResourceLoader, shouldWaitForLoadingOfRequestedResource loadingRequest: AVAssetResourceLoadingRequest) -> Bool {
+        return true
+    }
 
+    
     func imageProcessed(_ image: UIImage) {
         DispatchQueue.main.async {
             self.imageView.image = image
+            
+            // Decoder
+            
+            let time = self.output.itemTime(forHostTime: CACurrentMediaTime())
+            print(time)
+            
+            if self.output.hasNewPixelBuffer(forItemTime: time) {
+                let cvPixelBuffer = self.output.copyPixelBuffer(forItemTime: time, itemTimeForDisplay: nil)
+              let ciImage = CIImage(cvImageBuffer: cvPixelBuffer!)
+              let temporaryContext = CIContext();
+              
+              let width = CVPixelBufferGetWidth(cvPixelBuffer!)
+              let height = CVPixelBufferGetHeight(cvPixelBuffer!)
+              let cgRect = CGRect(x: 0,y: 0, width: width, height: height)
+              
+              let cgImage = temporaryContext.createCGImage(ciImage, from: cgRect)
+              
+              //let cgImage = try imageGenerator.copyCGImage(at: time, actualTime: &actualTime)
+                self.image_h264.image = UIImage(cgImage: cgImage!)
+              }
         }
     }
     
@@ -74,7 +130,7 @@ class ViewController: UIViewController, OpenCVCamDelegate {
     
     
     @IBAction func start(_ button: UIButton) {
-        //openCVWrapper.start()
+        openCVWrapper.start()
     }
     
     @IBAction func stop(_ button: UIButton) {
